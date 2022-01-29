49aacf0f8747351ade3888ad5de1687fd59bd269
agord1994 <91496518+agord1994@users.noreply.github.com>
2022-01-29 11:01:03 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ * 
+ * This class instantiates a linkedlist and queue. this is test class where we practice enqueing and dequeing strings odf people's names
+ * @author Andrew Gordon
+ *
+ * CS131ON
+ */
+public class Application {
+
+
+
+	public static void main(String[] args) {
+		
+		GenericLinkedList<String> newList=new GenericLinkedList<String>();
+	
+		Queue myQueue=new Queue(newList);
+		
+	
+
+		
+		myQueue.enqueue("Bill");
+		System.out.println(myQueue.toString());
+		
+		myQueue.enqueue("Alice");
+		
+		System.out.println(myQueue.toString());
+		
+		
+		myQueue.enqueue("Bob");
+		
+		System.out.println(myQueue.toString());
+		
+		myQueue.dequeue();
+		
+		System.out.println(myQueue.toString());
+		
+	
+
+		
+		
+		myQueue.enqueue("Jane");
+		System.out.println(myQueue.toString());
+		
+		
+		myQueue.enqueue("Hamad");
+		System.out.println(myQueue.toString());
+		
+		myQueue.dequeue();
+		System.out.println(myQueue.toString());
+		
+		myQueue.dequeue();
+		System.out.println(myQueue.toString());
+		
+		myQueue.enqueue("Jim");
+		System.out.println(myQueue.toString());
+		
+		myQueue.dequeue();
+		
+		System.out.println(myQueue.toString());
+		
+		myQueue.dequeue();
+		
+		System.out.println(myQueue.toString());
+		
+
+	}//end main method
+
+}//end Application class
